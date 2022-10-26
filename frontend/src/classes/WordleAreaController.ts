3697556a67d62bbf32d1f02bfda4fb1f20c6dba5
katelintharp <katelin.c.tharp@gmail.com>
2022-10-26 01:19:35 -0400

    Created frontend WordleArea and WordleAreaController

---
+++ frontend/src/classes/WordleAreaController.ts
@@ -0,0 +1,124 @@
+import EventEmitter from 'events';
+import _ from 'lodash';
+import TypedEmitter from 'typed-emitter';
+import { WordleArea as WordleAreaModel } from '../types/CoveyTownSocket';
+import PlayerController from './PlayerController';
+
+/**
+ * The events that the WordleAreaController emits to subscribers. These events
+ * are only ever emitted to local components (not to the townService).
+ */
+export type WordleAreaEvents = {
+  scoreChange: (newScore: number) => void;
+  occupantsChange: (newOccupants: PlayerController[]) => void;
+  playingChange: (newPlaying: boolean) => void;
+};
+
+/**
+ * A WordleAreaController manages the local behavior of a conversation area in the frontend,
+ * implementing the logic to bridge between the townService's interpretation of conversation areas and the
+ * frontend's. The WordleAreaController emits events when the conversation area changes.
+ */
+export default class WordleAreaController extends (EventEmitter as new () => TypedEmitter<WordleAreaEvents>) {
+  private _occupants: PlayerController[] = [];
+
+  private _model: WordleAreaModel;
+
+  /**
+   * Create a new WordleAreaController
+   * @param id
+   */
+  constructor(wordleAreaModel: WordleAreaModel) {
+    super();
+    this._model = wordleAreaModel;
+  }
+
+  /**
+   * The playback state - true indicating that the video is playing, false indicating
+   * that the video is paused.
+   */
+  public get isPlaying() {
+    return this._model.isPlaying;
+  }
+
+  /**
+   * The playback state - true indicating that a game is in progress,
+   * false indicating there is no active game
+   *
+   * Changing this value will emit a 'playingChange' event to listeners
+   */
+  public set isPlaying(isPlaying: boolean) {
+    if (this._model.isPlaying != isPlaying) {
+      this.emit('playingChange', isPlaying);
+      this._model.isPlaying = isPlaying;
+    }
+  }
+
+  /**
+   * The ID of this wordle area (read only)
+   */
+  get id() {
+    return this._model.id;
+  }
+
+  /**
+   * * The score of this wordle area (read only)
+   */
+  get score(): number {
+    return this._model.currentScore;
+  }
+
+  /**
+   * The score of this wordle area (read only)
+   */
+  set score(newScore: number) {
+    if (this._model.currentScore !== newScore) {
+      this.emit('scoreChange', newScore);
+    }
+    this._model.currentScore = newScore;
+  }
+
+  /**
+   * The list of occupants in this wordle area. Changing the set of occupants
+   * will emit an occupantsChange event.
+   */
+  set occupants(newOccupants: PlayerController[]) {
+    if (
+      newOccupants.length !== this._occupants.length ||
+      _.xor(newOccupants, this._occupants).length > 0
+    ) {
+      this.emit('occupantsChange', newOccupants);
+      this._occupants = newOccupants;
+    }
+  }
+
+  get occupants() {
+    return this._occupants;
+  }
+
+  /**
+   * A conversation area is empty if there are no occupants in it, or the topic is undefined.
+   */
+  isEmpty(): boolean {
+    return this._occupants.length === 0;
+  }
+
+  /**
+   * Return a representation of this WordleAreaController that matches the
+   * townService's representation and is suitable for transmitting over the network.
+   */
+  toWordleAreaModel(): WordleAreaModel {
+    return this._model;
+  }
+
+  /**
+   * Applies updates to this wordle area controller's model, setting the fields
+   * isPlaying and score
+   *
+   * @param updatedModel
+   */
+  public updateFrom(updatedModel: WordleAreaModel): void {
+    this.isPlaying = updatedModel.isPlaying;
+    this.score = updatedModel.currentScore;
+  }
+}
---
+++ frontend/src/components/Town/interactables/WordleArea.ts
@@ -0,0 +1,44 @@
+import WordleAreaController from '../../../classes/WordleAreaController';
+import Interactable, { KnownInteractableTypes } from '../Interactable';
+
+export default class WordleArea extends Interactable {
+  private _infoTextBox?: Phaser.GameObjects.Text;
+
+  private _wordleArea?: WordleAreaController;
+
+  getType(): KnownInteractableTypes {
+    return 'wordleArea';
+  }
+
+  private _showInfoBox() {
+    if (!this._infoTextBox) {
+      this._infoTextBox = this.scene.add
+        .text(
+          this.scene.scale.width / 2,
+          this.scene.scale.height / 2,
+          "You've found a Wordle game area!\nStart the game by pressing the spacebar.",
+          { color: '#000000', backgroundColor: '#FFFFFF' },
+        )
+        .setScrollFactor(0)
+        .setDepth(30);
+    }
+    this._infoTextBox.setVisible(true);
+    this._infoTextBox.x = this.scene.scale.width / 2 - this._infoTextBox.width / 2;
+  }
+
+  overlap(): void {
+    if (this._wordleArea === undefined) {
+      this._showInfoBox();
+    }
+  }
+
+  interact(): void {
+    if (this._wordleArea !== undefined) {
+      this._wordleArea.isPlaying = true;
+    }
+  }
+
+  overlapExit(): void {
+    this._infoTextBox?.setVisible(false);
+  }
+}
